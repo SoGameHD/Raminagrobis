c2cc3915d5e1d605b9e67ac4b788d74933820d9d
SoGameHD <erwann.leducq@epsi.fr>
2021-12-05 15:14:08 +0100

    Couche DTO Finalisé

---
+++ RaminagrobisDTO/Adherent_DTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaminagrobisDTO
+{
+    class Adherent_DTO
+    {
+        public string Societe { get; private set; }
+        public Boolean Civilite { get; private set; }
+        public string Nom { get; private set; }
+        public string Prenom { get; private set; }
+        public string Email { get; private set; }
+        public DateTime Date_adhesion { get; private set; }
+        public int? ID { get; set; }
+    }
+}
