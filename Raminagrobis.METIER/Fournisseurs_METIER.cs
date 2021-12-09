554273a2faea94754360ce00d3bd3dfbf773bf72
SoGameHD <erwann.leducq@epsi.fr>
2021-12-09 10:39:04 +0100

    Correctif Fournisseurs

--- Raminagrobis.METIER/Fournisseurs_METIER.cs
+++ Raminagrobis.METIER/Fournisseurs_METIER.cs
@@ -16,6 +16,7 @@ namespace Raminagrobis.METIER
         private int ID { get; set; }
 
         public Fournisseurs_METIER(string societe, bool civilite, string nom, string prenom, string email, string adresse, bool actif) => (Societe, Civilite, Nom, Prenom, Email, Adresse, Actif) = (societe, civilite, nom, prenom, email, adresse, actif);
+        public Fournisseurs_METIER(int id, string societe, bool civilite, string nom, string prenom, string email, string adresse, bool actif) => (ID, Societe, Civilite, Nom, Prenom, Email, Adresse, Actif) = (id, societe, civilite, nom, prenom, email, adresse, actif);
 
         #region Insert
         public void Insert()
