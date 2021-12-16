85bd1b4eb4f118b960bf9c070270d22b78680e77
SoGameHD <erwann.leducq@epsi.fr>
2021-12-16 10:24:48 +0100

    Initialisation API

---
+++ Raminagrobis.API/Controllers/Adherents_Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Raminagrobis.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Raminagrobis.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Adherents_Controller : ControllerBase
+    {
+       
+    }
+}
