d5dc2f2cb34edaef50c7eb1ce493a9ad8ea452fc
SoGameHD <erwann.leducq@epsi.fr>
2022-02-09 23:11:14 +0100

    page affichage WPF

---
+++ Raminagrobis.WPF/Produits.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Raminagrobis.WPF
+{
+    /// <summary>
+    /// Logique d'interaction pour Produits.xaml
+    /// </summary>
+    public partial class Produits : Page
+    {
+        public Produits()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnListe(object sender, RoutedEventArgs e)
+        {
+            InitializeComponent();
+            List<Produit> items = new List<Produit>();
+            items.Add(new Produit() { Reference = "Hey", Libelle = "ho", Marque = " Hoy", Actif = true, ID = 1 });
+            lvProduits.ItemsSource = items;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvProduits.ItemsSource);
+
+        }
+
+        public class Produit
+        {
+            public string Reference { get; set; }
+            public string Libelle { get; set; }
+            public string Marque { get; set; }
+            public bool Actif { get; set; }
+            public int ID { get; set; }
+
+        }
+    }
+}
